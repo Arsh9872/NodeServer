f28e6419671e360f4d0d897cec34ea0ef6bfcff8
Arsh9872 <arshpreetsingh0089@gmail.com>
2024-07-22 18:20:02 +0530

    Page not found last condition

--- app.js
+++ app.js
@@ -69,6 +69,10 @@ const app = http.createServer((req,res)=>{
         });
         
     }
+    else{
+        res.writeHead(404,{"Content-Type":"text/plain"});
+        res.end("Page Not Found !!");
+    }
 
 });
 

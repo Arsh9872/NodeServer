467f371447ebdf939289f8b98bb3707ce50653b6
Arsh9872 <arshpreetsingh0089@gmail.com>
2024-07-22 17:44:09 +0530

    different api

--- app.js
+++ app.js
@@ -15,6 +15,32 @@ const app = http.createServer((req,res)=>{
     console.log("requset url is ",req.url);
     console.log("request method is ", req.method);
 
+    if (req.url == "/") {
+        res.writeHead(200, {"Content-Type":"text/plain"});
+        res.end("This text is from backend app");
+        
+    }
+    else if (req.url == "/api/user") {
+        res.writeHead(200, {"Content-Type":"application/json"});
+
+        // cretae a java script object to return on this url
+
+        const user = {
+            name: "AP Singh",
+            gender: "Male"
+        };
+
+        // convert javascript to JSON
+        res.end(JSON.stringify(user));  
+
+        
+    }
+
+    else if (req.url == "/api/image") {
+        
+        
+    }
+
 });
 
 
@@ -23,3 +49,6 @@ const app = http.createServer((req,res)=>{
 app.listen(PORT,()=>{
     console.log(`Server listening on the port ${PORT}`);
 });
+
+
+// npm run dev == node --watch app.js  ::ref package.json
